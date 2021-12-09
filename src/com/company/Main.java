342159d2edc5082d5889d80df8174fedccf9645a
maxrule702 <maximoo2004@gmail.com>
2021-12-09 22:06:39 +0000

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,18 @@ package com.company;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
 
 
-    private static File bookfile = new File("bookList.txt");
 
-    private static ArrayList<String> students = new ArrayList<>();
+    private static File bookfile = new File("bookList.txt");
+    private static File LoginCheck = new File("LoginFile.txt");
+    private static Object username = "";
+
 
     public static String getInput(String prompt) {
         System.out.println(prompt);
@@ -21,15 +25,63 @@ public class Main {
     public static void CreateFile() {
         try {
             if (bookfile.createNewFile()) {
+                LoginCheck.createNewFile();
                 System.out.println("File created: " + bookfile.getName());
+                System.out.println("File created: " + LoginCheck.getName());
             } else {
-                System.out.println("File already exists.");
+                System.out.println("Files already exists.");
             }
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
+
+    public static void LoginCheck() {
+
+        System.out.println("would you like to login or register?");
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String userinput = input.nextLine();
+            if (userinput.equals("login")) {
+                Login();
+                break;
+            }
+            if (userinput.equals("register")) {
+                break;
+            }
+            if (!userinput.equals("login") || (!userinput.equals("register"))) {
+                System.out.println("please check your spelling");
+            }
+        }
+    }
+
+    public static void Login(){
+        Scanner in = null;
+        int check = 0;
+            try {
+                while(check !=1) {
+                    String userSearch = getInput("please enter your username");
+                    File file = new File("LoginFile.txt");
+                    in = new Scanner(file);
+                    while (in.hasNext()) {
+                        String line = in.nextLine();
+                        if (line.contains(userSearch)) {
+                            System.out.println("welcome back " + userSearch);
+                            username = userSearch;
+                            check++;
+                            break;
+                        }
+                        else{
+                            System.out.println("User not found");
+                        }
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                }
+            }
+
 
     public static String WritingToFile() throws IOException {
         String bookTitle = getInput("Enter your book's name");
@@ -90,7 +142,8 @@ public class Main {
 
     public static void main (String[]args) throws IOException {
 
-            //CreateFile();
+//          CreateFile();
+            LoginCheck();
             //WritingToFile();
             //ReadingFromFile();
             //searchingFile();
